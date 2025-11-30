965dbd0bc84d4867056751a946d1d1e1518c9f3f
Syrov-Egor <syrov_ev@mail.ru>
2025-11-30 13:50:25 +0300

    Dark mode added and random input placeholder function moved to scripts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import "./layout.css";
 	import favicon from "$lib/assets/favicon.ico";
+	import { DarkMode } from "flowbite-svelte";
 
 	let { children } = $props();
 </script>
+
+<DarkMode />
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
