f2abc33cd23e039aab493a4fdc56a48f672466d6
Syrov-Egor <syrov_ev@mail.ru>
2025-12-16 13:04:03 +0300

    Capacitor js included

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chemsynth.app',
+  appName: 'chemsynthcalc',
+  webDir: 'build'
+};
+
+export default config;
